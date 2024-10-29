4c48ed24aa375e9c39e9ef0d39f219b6d00bb7d4
Michael Mayer <michael.mayer1@edu.fh-joanneum.at>
2024-10-29 14:40:59 +0100

    Final version

--- UE3_Schulenote/Form1.Designer.cs
+++ UE3_Schulenote/Form1.Designer.cs
@@ -28,18 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            lblNote = new Label();
+            numNote = new NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)numNote).BeginInit();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(474, 205);
+            label1.Name = "label1";
+            label1.Size = new Size(72, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Note:";
+            // 
+            // lblNote
+            // 
+            lblNote.AutoSize = true;
+            lblNote.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lblNote.Location = new Point(838, 203);
+            lblNote.Name = "lblNote";
+            lblNote.Size = new Size(34, 45);
+            lblNote.TabIndex = 1;
+            lblNote.Text = "?";
+            lblNote.Click += lblNote_Click;
+            // 
+            // numNote
+            // 
+            numNote.Location = new Point(596, 203);
+            numNote.Name = "numNote";
+            numNote.Size = new Size(200, 39);
+            numNote.TabIndex = 2;
+            numNote.ValueChanged += numNote_ValueChanged;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1353, 761);
+            Controls.Add(numNote);
+            Controls.Add(lblNote);
+            Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numNote).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label lblNote;
+        private NumericUpDown numNote;
     }
 }
--- UE3_Schulenote/Form1.cs
+++ UE3_Schulenote/Form1.cs
@@ -4,7 +4,36 @@ namespace UE3_Schulenote
     {
         public Form1()
         {
+
             InitializeComponent();
+        }
+
+        private void lblNote_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numNote_ValueChanged(object sender, EventArgs e)
+        {
+            switch (numNote.Value)
+            {
+                case 1:
+                    lblNote.Text = "Sehr Gut";
+                    break;
+                case 2:
+                    lblNote.Text = "Gut";
+                    break; 
+                case 3:
+                    lblNote.Text = "Befridigend"; 
+                    break;
+                case 4:
+                    lblNote.Text = "Genügend";
+                    break;
+                case 5:
+                    lblNote.Text = "Nicht Genügend";
+                    break;
+                
+            }
         }
     }
 }
